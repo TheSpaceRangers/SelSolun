f211520cd4ab221d9ecee7fe14e21c7d9cf03bfe
TheSpaceRangers <tronel.charles@gmail.com>
2025-01-25 23:26:25 +0100

    Remove unused CorsFilter import from SecurityConfig

--- back/src/main/java/fr/thespacerangers/back/configs/SecurityConfig.java
+++ back/src/main/java/fr/thespacerangers/back/configs/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
