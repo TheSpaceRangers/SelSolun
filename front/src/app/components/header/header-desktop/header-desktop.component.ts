41ae507b04f71090c0f64876c7185771e177b840
TheSpaceRangers <tronel.charles@gmail.com>
2025-01-26 15:34:43 +0100

    Add responsive header component with desktop and mobile views

---
+++ front/src/app/components/header/header-desktop/header-desktop.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header-desktop',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './header-desktop.component.html',
+  styleUrl: './header-desktop.component.css'
+})
+export class HeaderDesktopComponent {
+
+}
