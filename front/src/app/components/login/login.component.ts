6b3469c480d88c191d1874a572ec7a432a90736e
TheSpaceRangers <tronel.charles@gmail.com>
2025-01-26 20:22:59 +0100

    Add login form functionality with validation and API call

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import { NgForOf, NgIf } from '@angular/common';
+
+import { LoginData } from '../../interfaces/login-data.interface';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  data: LoginData = {
+    email: '',
+    password: ''
+  };
 
+  errorMessages: string[] = [];
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  onSubmit(login: NgForm) {
+    if (login.valid && this.errorMessages.length === 0) {
+      this.authService.login(this.data).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (err) => {
+          console.log('Erreur lors de la connexion :', err);
+        }
+      });
+    } else {
+      this.errorMessages.length === 0 ? this.errorMessages.push('Veuillez remplir tous les champs.') : null;
+    }
+  }
 }
